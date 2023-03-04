55957024ffa33ea442f0a68809241a548542d064
devsince2021 <dev.ryu2021@gmail.com>
2023-03-04 22:15:12 +0900

    create get routes

---
+++ src/users/controllers/users/users.controller.ts
@@ -0,0 +1,11 @@
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { CreateUserDto } from 'src/users/dtos/createUser.dto';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  getUsers() {
+    return [{ username: 'Anson', email: 'anson@gmail.com' }];
+  }
+}
